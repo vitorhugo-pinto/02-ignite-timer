c73cbb7e74e124069e211821de3f17116170c4d6
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-05-13 19:12:10 -0300

    feat: colors and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>It's alive</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
