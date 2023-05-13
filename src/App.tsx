fb4a593890b7ceb08a44f2fc43fc8b6b577652cb
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-05-13 19:22:59 -0300

    chore: eslitn config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>It's alive</h1>
+      <h1>Its alive</h1>
 
       <GlobalStyle />
     </ThemeProvider>
