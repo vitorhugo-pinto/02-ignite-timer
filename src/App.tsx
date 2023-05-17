fbf6e3da009692db742cadb1ca1af2657944559d
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-05-17 15:41:38 -0300

    feat: setting up home and history routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Its alive</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
