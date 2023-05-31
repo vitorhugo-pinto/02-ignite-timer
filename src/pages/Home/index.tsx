ef0b70fa55abf345ef73da04ee6c64d1ab927f82
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-05-31 19:17:28 -0300

    feat: handle submit  with react hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+
 import {
   HomeContainer,
   FormContainer,
@@ -9,32 +11,50 @@ import {
   ProjectTimeInput,
 } from './styles'
 
+interface IProjectInputs {
+  projectName: string
+  projectTimerInMinutes: number
+}
+
 export function Home() {
+  const { register, handleSubmit, watch } = useForm<IProjectInputs>()
+
+  function handleCreateNewProjectTimer(data: IProjectInputs) {
+    console.log(data)
+    console.log(data.projectName)
+    console.log(data.projectTimerInMinutes)
+  }
+
+  const projectName = watch('projectName')
+  const isSubmitDisabled = !projectName
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewProjectTimer)} action="">
         <FormContainer>
-          <label htmlFor="project-name">Gonna work on</label>
+          <label htmlFor="projectName">Gonna work on</label>
           <ProjectNameInput
-            id="project-name"
-            list="naming-projects-suggestions"
+            id="projectName"
+            list="namingProjectsSuggestions"
             placeholder="Give your project a name"
+            {...register('projectName')}
           />
 
-          <datalist id="naming-projects-suggestions">
+          <datalist id="namingProjectsSuggestions">
             <option value="Name 1" />
             <option value="Name 2" />
             <option value="Name 3" />
           </datalist>
 
-          <label htmlFor="project-time-minutes">for</label>
+          <label htmlFor="projectTimerInMinutes">for</label>
           <ProjectTimeInput
-            id="project-time-minutes"
+            id="projectTimerInMinutes"
             type="number"
             placeholder="00"
             step={5}
             min={5}
             max={60}
+            {...register('projectTimerInMinutes', { valueAsNumber: true })}
           />
           <span>minutes</span>
         </FormContainer>
@@ -45,7 +65,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Start
         </StartCountdownButton>
