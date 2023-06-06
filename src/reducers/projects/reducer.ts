5ea0cf4b9338a2d3a7029b85112cf1c38809d1ac
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-06-06 13:12:55 -0300

    feat: changing set functions to immer syntax

--- src/reducers/projects/reducer.ts
+++ src/reducers/projects/reducer.ts
@@ -1,4 +1,5 @@
 import { ProjectsActionTypes } from './actions'
+import { produce } from 'immer'
 
 export interface IProjectModel {
   id: string
@@ -19,35 +20,57 @@ export function projectsReducer(state: IProjectsState, action: any) {
 
   switch (type) {
     case ProjectsActionTypes.CREATE_NEW_PROJECT:
-      return {
-        ...state,
-        projects: [...state.projects, payload.newProject],
-        activeProjectId: payload.newProject.id,
-      }
-    case ProjectsActionTypes.ABORT_PROJECT:
-      return {
-        ...state,
-        projects: state.projects.map((project) => {
-          if (project.id === state.activeProjectId) {
-            return { ...project, abortedDate: new Date() }
-          } else {
-            return project
-          }
-        }),
-        activeProjectId: null,
-      }
-    case ProjectsActionTypes.FINISHED_PROJECT:
-      return {
-        ...state,
-        projects: state.projects.map((project) => {
-          if (project.id === state.activeProjectId) {
-            return { ...project, finishedDate: new Date() }
-          } else {
-            return project
-          }
-        }),
-        activeProjectId: null,
-      }
+      return produce(state, (draft) => {
+        draft.projects.push(payload.newProject)
+        draft.activeProjectId = payload.newProject.id
+      })
+    // return {
+    //   ...state,
+    //   projects: [...state.projects, payload.newProject],
+    //   activeProjectId: payload.newProject.id,
+    // }
+    case ProjectsActionTypes.ABORT_PROJECT: {
+      const currentProjectIndex = state.projects.findIndex((project) => {
+        return project.id === state.activeProjectId
+      })
+      if (currentProjectIndex === -1) return state
+      return produce(state, (draft) => {
+        draft.activeProjectId = null
+        draft.projects[currentProjectIndex].abortedDate = new Date()
+      })
+    }
+    // return {
+    //   ...state,
+    //   projects: state.projects.map((project) => {
+    //     if (project.id === state.activeProjectId) {
+    //       return { ...project, abortedDate: new Date() }
+    //     } else {
+    //       return project
+    //     }
+    //   }),
+    //   activeProjectId: null,
+    // }
+    case ProjectsActionTypes.FINISHED_PROJECT: {
+      const currentProjectIndex = state.projects.findIndex((project) => {
+        return project.id === state.activeProjectId
+      })
+      if (currentProjectIndex === -1) return state
+      return produce(state, (draft) => {
+        draft.activeProjectId = null
+        draft.projects[currentProjectIndex].finishedDate = new Date()
+      })
+      // return {
+      //   ...state,
+      //   projects: state.projects.map((project) => {
+      //     if (project.id === state.activeProjectId) {
+      //       return { ...project, finishedDate: new Date() }
+      //     } else {
+      //       return project
+      //     }
+      //   }),
+      //   activeProjectId: null,
+      // }
+    }
     default:
       return state
   }
